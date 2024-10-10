8fe65ca0d19b7eaa5817656e1aaef3c3a0f482b8
Vnsh Kumar <vnshkumar21@gmail.com>
2024-10-11 00:07:12 +0530

    level selection

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -9,20 +9,53 @@ public class Lobby : MonoBehaviour
 
     public Button StartButton;
     public Button QuitButton;
+    public Button backButton;
+    public Button Level1;
+    public Button Level2;
+    public GameObject LevelSelector;
+    public GameObject LobbyMenu;
 
     private void Start()
     {
-        StartButton.onClick.AddListener(PlayGame);
+        StartButton.onClick.AddListener(LevelSelect);
         QuitButton.onClick.AddListener(QuitGame);
+        Level1.onClick.AddListener(PlayLevel1);
+        Level2.onClick.AddListener(PlayLevel2);
+        LevelSelector.SetActive(false);
+        backButton.onClick.AddListener(backSelect);
+       
     
     }
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("Level1");
-    }
+    
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LevelSelect()
+    {
+        LobbyMenu.SetActive(false);
+        LevelSelector.SetActive(true);
+        backButton.gameObject.SetActive(true);
+        Level1.onClick.AddListener(PlayLevel1);
+        Level2.onClick.AddListener(PlayLevel2);
+    }
+
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void backSelect()
+    {
+        LobbyMenu.SetActive(true);
+        LevelSelector.SetActive(false);
+        backButton.gameObject.SetActive(false);
     }
 }
